23159be7d3dc4ba4a2060b90ffe64697d68c7295
dag-bag <virenderkumar23435@gmail.com>
2023-09-08 20:19:45 +0530

    git init

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,5 +1,9 @@
 import GoogleProvider from "next-auth/providers/google";
-import { signInWithOAuth, getUserByEmail, signUpWithCredentials } from "./modules";
+import {
+  signInWithOAuth,
+  getUserByEmail,
+  signUpWithCredentials,
+} from "./modules";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
@@ -8,21 +12,21 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "Email", required: true },
-        password: { label: "Password", type: "password", placeholder: "Password", required: true },
-      },
-      async authorize(credentials, req) {
-       
-        const { email, password } = credentials;
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
+    //     email: { label: "Email", type: "email", placeholder: "Email", required: true },
+    //     password: { label: "Password", type: "password", placeholder: "Password", required: true },
+    //   },
+    //   async authorize(credentials, req) {
 
-        const user = await signUpWithCredentials({ email, password });
-        // console.log({user});
-        return user;
-      },
-    }),
+    //     const { email, password } = credentials;
+
+    //     const user = await signUpWithCredentials({ email, password });
+    //     // console.log({user});
+    //     return user;
+    //   },
+    // }),
   ],
   pages: {
     signIn: "/signin", //
